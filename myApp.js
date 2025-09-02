9f6ceffd1d8575e551153e145d6c59c4b1e964a5
Waldo <waldito.07@gmail.com>
2025-09-02 19:59:11 -0400

    myApp.js

--- myApp.js
+++ myApp.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGO_URI; // defined in .env or host env
+if (!uri) {
+  console.warn('Warning: MONGO_URI not set yet.');
+}
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Exporting mongoose is optional; it can help in other files
+module.exports = mongoose;
 
